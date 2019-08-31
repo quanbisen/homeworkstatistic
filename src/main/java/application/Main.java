1d82138285e3c9bd0058f7fafaafe340f800f541
lollipop <1769128867@qq.com>
2019-08-31 12:45:27 +0800

    Add some annotation.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,14 +8,25 @@ import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+/**
+ * 文件名:Main
+ * 说明：程序的入口类
+ * @author super lollipop
+ * @date 2019-8-30
+ */
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //装载布局
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        //设置窗体标题
         primaryStage.setTitle("HomeworkStatistic");
+        //设置窗体的图标
         primaryStage.getIcons().add(new Image("/image/ApplicationIcon.png"));
+        //设置舞台场景
         primaryStage.setScene(new Scene(root));
+        //主舞台显示
         primaryStage.show();
 
         // 获取屏幕可视化的宽高（Except TaskBar），把窗体设置在可视化的区域中间
